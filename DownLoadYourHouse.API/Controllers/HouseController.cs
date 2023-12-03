ecb58eb6e98c3fac9d50cc0682d87c65ceb7b02e
Mandurang <56937341+Mandurang@users.noreply.github.com>
2023-12-03 19:00:46 +0000

    Added new Model

---
+++ DownLoadYourHouse.API/Controllers/HouseController.cs
@@ -0,0 +1,58 @@
+﻿using DownLoadYourHouse.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DownLoadYourHouse.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HouseController : ControllerBase
+    {
+        private static readonly Dictionary<Guid, House> house = new();
+
+        private readonly ILogger<HouseController> _logger;
+
+        public HouseController(ILogger<HouseController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(house);
+        }
+
+        [HttpPost]
+        public IActionResult Post(House house)
+        {
+            var id = Guid.NewGuid();
+            HouseController.house.Add(id, house);
+            return base.Ok(HouseController.house);
+        }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult Put([FromRoute] Guid id, [FromBody] House house)
+        {
+            var existingHouse = HouseController.house.GetValueOrDefault(id);
+            if (existingHouse != null)
+            {
+                HouseController.house[id] = house;
+            }
+            return base.Ok(HouseController.house);
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var existingHouse = house.GetValueOrDefault(id);
+            return Ok(existingHouse);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            house.Remove(id);
+            return Ok(house);
+        }
+    }
+}
---
+++ DownLoadYourHouse.API/Model/House.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DownLoadYourHouse.API.Model
+{
+    public class House
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Reference { get; set; }
+        public decimal GPSLatitude { get; set; }
+        public decimal GPSLongitude { get; set;}
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string Town { get; set; }
+        public string Neighborhood { get; set; }
+        public string Zone { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public decimal Price { get; set; }
+        public BusinessType BusinessType { get; set; }
+        public Availability Availability { get; set; }
+        public Status Status { get; set; }
+        public DateTime Created { get; set; }
+        public string Description { get; set; }
+        public int IdOwner { get; set; }
+        public string Owner { get; set; }
+        public int IdAgent { get; set; }
+        public string Agent { get; set; }
+    }
+
+    public enum BusinessType
+    {
+
+    }
+
+    public enum Availability
+    {
+        
+    }
+
+    public enum Status
+    {
+
+    }
+}
